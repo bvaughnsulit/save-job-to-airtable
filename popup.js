308e2066c3f8a6775552dcae097290ce1e5c1181
bvaughnsulit <bvaughnsulit@gmail.com>
2021-07-27 13:32:13 -0700

    it works

--- popup.js
+++ popup.js
@@ -1,11 +1,38 @@
-let changeColor = document.getElementById('changeColor');
+let submitButton = document.getElementById('submit');
 
-changeColor.onclick = function(element) {
-  alert("hello")
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
+submitButton.onclick = function(element) {
+  const position = document.getElementById('position').value;
+  const company = document.getElementById('company').value;
+  const interest = document.getElementById('interest').value;
+  
+  const body = {
+    "fields": {
+      "Title": position,
+      //"URL": tab.url,
+      "Company": [
+        company
+      ],
+      "Interest": interest,
+    },
+    "typecast": true
+  }
+
+  postToAirtable(body);
+
 };
+
+function postToAirtable(postBody){
+  var xhr = new XMLHttpRequest();
+
+  xhr.open("POST", 'https://api.airtable.com/v0/appjp1mWBsWIWgJse/Open%20Positions?api_key=keylFAs2bPeYqhYFt', true);
+
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.onreadystatechange = function(err) {
+      if (this.readyState === XMLHttpRequest.DONE) {
+        const success = JSON.parse(xhr.responseText).fields.Title
+        alert(`${success} created successfully!`)
+      } 
+  }
+  xhr.send(JSON.stringify(postBody));
+}
